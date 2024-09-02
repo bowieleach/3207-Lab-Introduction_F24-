a535cfbfbb5d95a3447bf9c43ea51f75eb7e7321
Bowie <tul33233@temple.edu>
2024-09-01 23:34:44 -0400

    Removed comment in random.c that was left in on accident

--- random.c
+++ random.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-//generate a random number between 1 and 26 for all letters of the alphabet
 char randchar() {
     char letters[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int randInt = rand() % 26;
